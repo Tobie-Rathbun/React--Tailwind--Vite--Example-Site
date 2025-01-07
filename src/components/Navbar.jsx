2d5bcf748d7c92f002c573dc69f53a9d35c2cf62
Tobie-Rathbun <tsrathbun@gmail.com>
2025-01-07 11:18:24 -0700

    ul links on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,8 @@ const Navbar = () => {
         py-5 fixed 
         top-0 z-20 
         bg-primary
-      `}
+        `
+      }
     >
       <div className='
         w-full 
@@ -33,7 +34,7 @@ const Navbar = () => {
             flex 
             items-center 
             gap-2
-          '
+            '
           onClick={() => {
             setActive("");
             window.scrollTo(0, 0)
@@ -42,20 +43,37 @@ const Navbar = () => {
             <img src={logo} alt="logo" className='
               w-9 
               h-9 
-              object-contain' 
+              object-contain
+              ' 
             />
             <p className='
               text-white 
               text-[18px] 
               cursor-pointer
-            '>
+              '
+            >
               Tobie  
               <span className='
                 sm:block 
                 hidden
-              '> | Full-stack JavaScript Development</span>
+                '
+              > | Full-stack JavaScript Development</span>
             </p>
           </Link>
+          <ul className='
+            list-none
+            hidden
+            sm:flex 
+            flex-row 
+            gap-10
+            '
+          >
+            {navLinks.map((link) => (
+              <li>
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            ))}
+          </ul>
       </div>
     </nav>
   )
