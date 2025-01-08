6635e82d4920e70d7bd407145065587e68b19d59
Tobie-Rathbun <tsrathbun@gmail.com>
2025-01-08 11:32:50 -0700

    Implemented basic About elements

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-parallax-tilt"
+import { motion } from "framer-motion";
 
-const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const About = () => (
+  <>
+    <motion.div
+      variants={textVariant()}
+    >
+      <p className={styles.sectionSubText}
+      >Introduction</p>
+      <h2 className={styles.sectionHeadText}
+      >Overview</h2>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Software bio
+      </motion.p>
+    </motion.div>
+  </>
+);
 
 export default About
