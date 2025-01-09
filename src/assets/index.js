02b23abdc15538b2f60829aed49c9a583854904d
Tobie-Rathbun <tsrathbun@gmail.com>
2025-01-08 22:15:40 -0700

    updated work icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,7 +9,6 @@ import close from "./close.svg";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
@@ -21,10 +20,11 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import manpower from "./company/manpower.png";
+import flutter from "./company/flutter.png";
+import php from "./company/php.png";
+import illustrator from "./company/illustrator.png";
+
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -41,7 +41,6 @@ export {
   close,
   css,
   docker,
-  figma,
   git,
   html,
   javascript,
@@ -52,10 +51,10 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  manpower,
+  php,
+  flutter,
+  illustrator,
   carrent,
   jobit,
   tripguide,
