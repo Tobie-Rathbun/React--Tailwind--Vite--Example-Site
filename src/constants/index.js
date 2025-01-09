a28ef94adbd7b0a61c36b02ff6e8f7b39b8d6c84
Tobie-Rathbun <tsrathbun@gmail.com>
2025-01-09 00:15:07 -0700

    Added testimonials

--- src/constants/index.js
+++ src/constants/index.js
@@ -164,26 +164,26 @@ import {
   const testimonials = [
     {
       testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
-      designation: "CFO",
-      company: "Acme Co",
+        "Tobie really did the dang thing. I am impressed with his tenacity.",
+      name: "Anita Joweb",
+      designation: "CTO",
+      company: "Notta Inc.",
       image: "https://randomuser.me/api/portraits/women/4.jpg",
     },
     {
       testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
+        "I've never met a web developer who is as driven as Tobie.",
+      name: "Rocky Rhode",
+      designation: "CEO",
+      company: "Rocky's Ice Cream Co",
       image: "https://randomuser.me/api/portraits/men/5.jpg",
     },
     {
       testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
+        "After Tobie optimized our website SEO, our traffic increased by 50%. We can't thank them enough!",
+      name: "Nadia Brough",
       designation: "CTO",
-      company: "456 Enterprises",
+      company: "NE Enterprises",
       image: "https://randomuser.me/api/portraits/women/6.jpg",
     },
   ];
