0f2ed108e3c80a6837239bc1a947b8f2eae5aba2
Tobie-Rathbun <tsrathbun@gmail.com>
2025-01-08 21:01:46 -0700

    empty about links with tilt animation properly applied

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,19 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+const ServiceCard = ({ index, title, icon}) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+
+      </motion.div>
+    </Tilt>
+  )
+}
+
 const About = () => (
   <>
     <motion.div
@@ -15,14 +28,20 @@ const About = () => (
       >Introduction</p>
       <h2 className={styles.sectionHeadText}
       >Overview</h2>
+    </motion.div>
 
-      <motion.p
+    <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
         Software bio
-      </motion.p>
-    </motion.div>
+    </motion.p>
+
+    <div className='mt-20 flex flex-wrap gap-10'>
+      {services.map((services, index) => (
+        <ServiceCard key={services.title} index={index} {...services} />
+      ))}
+    </div>
   </>
 );
 
