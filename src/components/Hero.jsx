42f92a5e6237b0b34f01d9513ee0b7c623ac1b96
Tobie-Rathbun <tsrathbun@gmail.com>
2025-01-15 10:43:45 -0700

    changed link title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#ad4276]'>Tobie</span>
+            <span className='text-[#ad4276]'>Tobie</span> Developer
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop JavaScript components <br className='sm:block hidden' />
